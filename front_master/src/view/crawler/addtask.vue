0279b71380dac9a618d6f114e3fd807a224204e1
Fkz011014@ <479875582@qq.com>
2023-08-01 00:56:23 +0800

    1

---
+++ front_master/src/view/crawler/addtask.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-form :model="form">
+    <a-form-item label="任务名称">
+      <a-input v-model="form.name" />
+    </a-form-item>
+    <a-button htmlType="submit">提交添加</a-button>
+  </a-form>
+</template>
+
+<script lang="ts">
+import { addTask } from 'front_master/src/api/scrawl.js'
+
+export default {
+  data() {
+    return {
+      form: {}
+    }
+  },
+
+  methods: {
+    async onSubmit() {
+      await addTask(this.form)
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
