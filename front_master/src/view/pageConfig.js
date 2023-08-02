383a235538edc2a294af5ea2e936adfae8d62d8a
Indigo-gg <3123986908@qq.com>
2023-08-02 20:10:29 +0800

    template  2/3

--- front_master/src/view/pageConfig.js
+++ front_master/src/view/pageConfig.js
@@ -2,17 +2,16 @@ import storage from "@/store/store";
 import {HAS_LOGIN, USER_INFO} from "@/store/constant";
 import router from "@/router";
 
-export const route = ()=>{
+export const route = () => {
 
-    let page=[]
+    let page = []
     // console.log('routes',router.getRoutes())
-    router.getRoutes().forEach(route=>{
-        if(route.meta.isShow)
-        {
+    router.getRoutes().forEach(route => {
+        if (route.meta.isShow) {
             page.push({
-                icon:route.meta.icon,
-                title:route.meta.name,
-                key:route.name
+                icon: route.meta.icon,
+                title: route.meta.name,
+                key: route.name
             })
         }
 
@@ -21,18 +20,18 @@ export const route = ()=>{
 }
 
 
-export const user={
-    name:'User',
-    phone:'123456',
-    id:undefined,
-    userInfo:undefined,
-    removeLoginState:()=>{
+export const user = {
+    name: 'User',
+    phone: '123456',
+    id: undefined,
+    userInfo: undefined,
+    removeLoginState: () => {
         storage.remove(HAS_LOGIN)
         router.go(0)
     }
 }
 
-export const userAvatar=[
+export const userAvatar = [
     require('@/assets/user.png'),
     require('@/assets/user_1.png'),
     require('@/assets/user_2.png'),
@@ -42,12 +41,12 @@ export const userAvatar=[
 ]
 
 
-export const app={
+export const app = {
 
-    appName:'信息检索管理平台',
-    copyRight:'远光软件股份有限公司',
+    appName: '信息检索管理平台',
+    copyRight: '远光软件股份有限公司',
 }
-export const tags=[
+export const tags = [
     {text: '算法', value: 0},
     {text: '后端', value: 1},
     {text: '前端', value: 2},
@@ -59,7 +58,7 @@ export const tags=[
     {text: '其他', value: 8},
 ]
 
-export const colorTagMap={
+export const colorTagMap = {
     1: 'blue',
     2: 'orange',
     3: 'green',
@@ -71,15 +70,85 @@ export const colorTagMap={
     0: 'green',
 }
 
-export  const getTags=(tag)=>{
-    let r= tags.find(i=>{
-        return i.value===tag
+export const template = {
+    map: {
+        button:[
+            {
+                text:'圆形按钮',
+                value:0
+            },
+            {
+                text:'方形按钮',
+                value:1
+            },
+            {
+                text:'白底按钮',
+                value:2
+            }
+        ],
+        color:[
+            {
+                text:'黄色',
+                value:'#FEFEBE'
+            },
+            {
+                text:'绿色',
+                value:'#EDF8BB'
+            },
+            {
+              text:'白色',
+              value:'#E8FFFB'
+            },
+            {
+                text:'蓝色',
+                value:'#C3E7FE'
+            },
+            {
+                text:'紫色',
+                value:'#F5E8FF'
+            }
+        ],
+        searchs:[
+            {
+                text:'高级搜索',
+                value:'高级搜索'
+            },
+            {
+                text:'初级搜索',
+                value:'初级搜索'
+            }
+        ],
+        style:[
+            {
+                text:'一号字体',
+                value:'24px'
+            },
+            {
+                text:'二号字体',
+                value:'18px'
+            },
+            {
+                text:'三号字体',
+                value:'14px'
+            },
+            {
+                text:'四号字体',
+                value:'9px'
+            }
+        ]
+    }
+}
+
+
+export const getTags = (tag) => {
+    let r = tags.find(i => {
+        return i.value === tag
     })
     return r.text
 }
 
 
-export function resetUserState(){
+export function resetUserState() {
     storage.remove(HAS_LOGIN)
     storage.remove(USER_INFO)
     router.go(0)
--- front_master/src/view/template/index.vue
+++ front_master/src/view/template/index.vue
@@ -1,51 +1,166 @@
 <template>
   <div>
-    <a-button type="primary" round @click="addTemp">添加</a-button>
+    <div class="add-btn">
+      <a-button type="primary" size="large" shape="circle" @click="addTemp">
+        <template #icon>
+          <PlusOutlined/>
+        </template>
+      </a-button>
+    </div>
+
     <a-table :columns="columns" :dataSource="templates" row-key="id" bordered>
       <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'avatarUrl'">
-          <a-avatar :src="record.avatarUrl"/>
+        <template v-if="column.dataIndex === 'createTime'">
+          {{ DateToString(record.createTime) }}
         </template>
+        <template v-else-if="column.dataIndex==='color'">
+          <a-tag :color="record.color">
+                <span style="color: #999999">{{record.color}}</span>
+          </a-tag>
+        </template>
+        <template v-else-if="column.dataIndex==='isPublic'">
+         <a-tag color="red" v-if="record.isPublic===1">
+           已公开
+         </a-tag>
+          <a-tag color="blue" v-else>
+            未公开
+          </a-tag>
+        </template>
+        <template v-else-if="column.dataIndex==='style'">
+         <span :style="{fontSize:record.style}">{{record.style}}</span>
+        </template>
+        <template v-else-if="column.dataIndex === 'updateTime'">
+          {{ DateToString(record.updateTime) }}
+        </template>
+
+      </template>
+      <template #action>
+        <div class="flex-row-space-around">
+          <a-button type="link" style="color: red">删除</a-button>
+          <a-button type="link">编辑</a-button>
+        </div>
+
       </template>
     </a-table>
-    <a-modal v-model:visible="visible" @ok="submitForm" :confirm-loading="confirmLoading">
-      <a-form :ref="formRef">
-        <a-form-item label="搜索关键词" :has-feedback="true" :validate-status="validateStatus" :help="helpMessage">
-          <a-input v-model:value="formData.searchs" />
+    <a-modal v-model:visible="visible" @ok="submitForm" :confirm-loading="confirmLoading" ok-text="确认"
+             cancel-text="取消">
+      <a-form ref="formRef" :model="formData" style="padding: 1em">
+        <!--        <a-form-item name="" v-for="item in columns" :key="item.key">-->
+        <!--          -->
+        <!--        </a-form-item>-->
+        <a-form-item name="searchs" :label="getColByName('searchs')" :has-feedback="true"
+                     :validate-status="validateStatus" :help="helpMessage">
+          <a-select
+              v-model:value="formData.searchs"
+              style="width: 320px"
+          >
+            <a-select-option v-for="option in TMap.searchs" :value="option.value" :key="option.value">
+              {{ option.text }}
+            </a-select-option>
+          </a-select>
         </a-form-item>
-        <a-form-item label="按钮类型">
-          <a-radio-group v-model:value="formData.button">
-            <a-radio-button :value="0">普通按钮</a-radio-button>
-            <a-radio-button :value="1">特殊按钮</a-radio-button>
-          </a-radio-group>
+        <a-form-item name="color" :label="getColByName('color')" :has-feedback="true" :validate-status="validateStatus"
+                     :help="helpMessage">
+          <a-select
+              v-model:value="formData.color"
+              style="width: 320px"
+          >
+            <a-select-option v-for="option in TMap.color" :value="option.value" :key="option.value"
+                             style="text-align: center">
+              <a-tag :color="option.value">
+                <span style="color: #999999">
+                  {{ option.text }}
+                </span>
+              </a-tag>
+            </a-select-option>
+          </a-select>
         </a-form-item>
-        <!-- 其他表单项省略 -->
+        <a-form-item name="style" :label="getColByName('style')" :has-feedback="true" :validate-status="validateStatus"
+                     :help="helpMessage">
+          <a-select
+              v-model:value="formData.style"
+              style="width: 320px"
+          >
+            <a-select-option v-for="option in TMap.style" :value="option.value" :key="option.value"
+                             style="text-align: center">
+              <span :style="{fontSize:option.value}">{{ option.text }}</span>
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item name="button" :label="getColByName('button')">
+          <a-select
+              v-model:value="formData.button"
+              style="width: 320px;"
+          >
+            <a-select-option v-for="option in TMap.button" :value="option.value" :key="option.value"
+                             style="text-align: center">
+              <a-button :style="{fontSize:option.value}" size="small" shape="round" v-if="option.value===0" type="primary">
+                {{ option.text }}
+
+                <template #icon>
+                  <SearchOutlined></SearchOutlined>
+                </template>
+              </a-button>
+              <a-button :style="{fontSize:option.value}" size="small" v-else-if="option.value===1" type="primary">
+                {{ option.text }}
+                <template #icon>
+                  <SearchOutlined></SearchOutlined>
+
+                </template>
+              </a-button>
+              <a-button :style="{fontSize:option.value}" size="small" v-else-if="option.value===2" type="dashed">
+                {{ option.text }}
+                <template #icon>
+                  <SearchOutlined></SearchOutlined>
+
+                </template>
+              </a-button>
+              <a-button :style="{fontSize:option.value}" size="small" v-else>未知按钮</a-button>
+
+            </a-select-option>
+          </a-select>
+        </a-form-item>
       </a-form>
+      <div class="bottom">
+        {{formData.userAccount}}最后一次更新于
+        <span v-if="formData.updateTime" v-text="DateToString(formData.updateTime)">
+        </span>
+        <span v-else>
+          {{new Date().toLocaleDateString()}}
+        </span>
+      </div>
     </a-modal>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+import {nextTick, ref} from "vue";
 import {message} from "ant-design-vue";
-import {deleteTemplate, getTemplateList} from "@/api/template";
+import {addTemplate, deleteTemplate, getTemplateList, updateTemplate} from "@/api/template";
+import {PlusOutlined, SearchOutlined} from "@ant-design/icons-vue";
+import {template} from "@/view/pageConfig";
+import {DateToString} from "@/utils/date";
 
 export default {
   name: "index",
-  setup(){
+  components: {
+    PlusOutlined,
+    SearchOutlined
+  },
+  setup() {
     const templates = ref([]); // 模板列表数据
     const visible = ref(false); // 控制新增/编辑弹窗的显示
     const confirmLoading = ref(false); // 控制新增/编辑弹窗确定按钮的loading状态
-    const formRef = ref(null); // 表单引用
-    const requestParams={
-      size:50,
-      current:1
+    const formRef = ref(); // 表单引用
+    const requestParams = {
+      size: 50,
+      current: 1
     }
     const formData = ref({
-      button: 0,
+      button: 1,
       color: '',
       createTime: '',
-      id: 0,
+      id: undefined,
       isDeleted: 0,
       isPublic: 0,
       searchs: '',
@@ -56,36 +171,83 @@ export default {
     const validateStatus = ref(''); // 表单验证状态
     const helpMessage = ref(''); // 表单验证提示信息
 
+
     // 表格列配置
     const columns = [
       {
-        title: '搜索关键词',
+        key: 1,
+        title: '搜索模式',
         dataIndex: 'searchs',
       },
       {
+        key: 2,
+        title: '按钮类型',
+        dataIndex: 'button',
+      },
+      {
+        key: 3,
+        title: '页面颜色',
+        dataIndex: 'color',
+      },
+      {
+        key: 4,
+        title: '是否公开',
+        dataIndex: 'isPublic',
+      },
+      {
+        key: 5,
+        title: '发布用户',
+        dataIndex: 'userAccount',
+      },
+      {
+        key: 6,
+        title: '字体大小',
+        dataIndex: 'style',
+      },
+
+      {
+        key: 7,
+        title: '更新时间',
+        dataIndex: 'updateTime',
+      }, {
+        key: 8,
+        title: '创建时间',
+        dataIndex: 'createTime',
+      },
+
+
+      {
+        key: 0,
         title: '操作',
         dataIndex: 'action',
         width: '10%',
-        slots: { customRender: 'action' },
+        slots: {customRender: 'action'},
       },
     ];
-
+    const getColByName = (name) => {
+      return columns.find(i => {
+        return i.dataIndex === name
+      }).title
+    }
     // 页面加载时获取模板列表
     const getListTemp = () => {
-      getTemplateList(requestParams).then(res=>{
-        templates.value=res.data.data.records
+      getTemplateList(requestParams).then(res => {
+        templates.value = res.data.data.records
       })
     };
 
     // 新增模板
     const addTemp = () => {
-      formRef.value.resetFields(); // 重置表单
+      console.log('表单对象', formRef.value)
       visible.value = true; // 显示新增/编辑弹窗
+      nextTick(() => {
+        formRef.value.resetFields(); // 重置表单
+      })
     };
 
     // 编辑模板
     const editTemp = (record) => {
-      formData.value = { ...record };
+      formData.value = {...record};
       visible.value = true; // 显示新增/编辑弹窗
     };
 
@@ -95,11 +257,22 @@ export default {
           .validate()
           .then(() => {
             confirmLoading.value = true; // 开启确定按钮的loading状态
-            const payload = { ...formData.value };
+            const payload = {...formData.value};
             if (payload.id) {
-              // 调用接口updateTemplate更新模板
+              updateTemplate(payload).then(res=>{
+                if(res.data.code===0){
+                  formData.value.id=undefined
+                  getListTemp();
+                }
+              })
             } else {
-              // 调用接口addTemplate新增模板
+              addTemplate(payload).then(res=>{
+                if(res.data.code===0){
+                  message.success('新增成功')
+                  confirmLoading.value=false
+                  getListTemp();
+                }
+              })
             }
           })
           .catch(() => {
@@ -112,9 +285,9 @@ export default {
     const deleteTemp = (record) => {
 
       deleteTemplate({
-        templateId:record.id
-      }).then(res=>{
-        if(res.data.code===0)
+        templateId: record.id
+      }).then(res => {
+        if (res.data.code === 0)
           message.info('删除模板成功')
         else
           message.error('删除模板失败')
@@ -144,11 +317,19 @@ export default {
       submitForm,
       deleteTemp,
       handlePageChange,
+      getColByName, TMap: template.map,
+      DateToString:(date)=>{
+        return DateToString(date)
+      }
     };
   }
 }
 </script>
 
 <style scoped>
-
+.add-btn {
+  position: absolute;
+  bottom: 10vw;
+  right: 3vw;
+}
 </style>
